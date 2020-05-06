911506b6d03e62ce8bcf99c9cf8d64633322d323
Marcin Dobosz <43973855+mdobosz-isp@users.noreply.github.com>
2020-05-06 16:33:55 -0700

    feat: add minimize-comment action (#1)

---
+++ minimize-comments/README.md
@@ -0,0 +1,40 @@
+# minimize-comments javascript action
+
+This action minimizes all comments by the specified user which include the given string in the comment body.
+It can be used as part of a workflow where a bot emits comments on every PR push. This action should be run
+at the beginning of the workflow to minimize all previous comments so that only the latest comments remain
+visible.
+
+## Inputs
+
+### `github-token`
+
+**Required** The GitHub API token to use.
+
+### `by-author`
+
+**Required** The id of the comment author to match.
+
+### `body-includes`
+
+**Required** Newline-separated list of strings that must be present in the body of the comment for it to be minimized. If at least one of the strings in the body is found the comment will be minimized.
+
+## Outputs
+
+None
+
+## Example usage
+
+```yaml
+uses: iStreamPlanet/github-actions/minimize-comments@master
+with:
+  github-token: ${{secrets.GITHUB_TOKEN}}
+  by-author: github-actions
+  body-includes: |
+    terraform plan
+    terraform validate
+```
+
+# Development
+
+To build the action for distribution run `npm run build` before committing your changes.
---
+++ minimize-comments/index.js
@@ -0,0 +1,70 @@
+import { getInput, setFailed } from "@actions/core";
+import { context, GitHub } from "@actions/github";
+
+run();
+
+async function run() {
+  try {
+    const token = getInput("github-token", { required: true });
+    const bodyIncludesInput = getInput("body-includes", { required: true });
+    const bodyStrings = bodyIncludesInput.split("\n").map(s => s.trim()).filter(s => s.length > 0)
+    const byAuthor = getInput("by-author", { required: true });
+
+    const github = new GitHub(token);
+
+    let execute = true;
+    let nextCursor = undefined;
+    while (execute) {
+      const result = await github.graphql(
+        `query FindComments($owner: String!, $repo: String!, $number: Int!, $nextCursor: String) {
+          repository(owner: $owner, name: $repo) {
+            pullRequest(number: $number) {
+              comments(first: 50, after: $nextCursor) {
+                pageInfo {
+                  endCursor
+                  hasNextPage
+                  hasPreviousPage
+                  startCursor
+                }
+                nodes {
+                  isMinimized
+                  id
+                  author {
+                    login
+                  }
+                  body
+                }
+              }
+            }
+          }
+        }`,
+        { ...context.issue, nextCursor }
+      );
+      const {
+        pageInfo,
+        nodes: comments,
+      } = result.repository.pullRequest.comments;
+      for (const comment of comments) {
+        if (
+          !comment.isMinimized &&
+          comment.author.login === byAuthor &&
+          bodyStrings.some(s => comment.body.includes(s))
+        ) {
+          console.log("Minimizing " + comment.id);
+          await github.graphql(
+            `mutation MarkCommentOutdated($commentId: ID!) {
+              minimizeComment(input: {subjectId: $commentId, classifier: OUTDATED}) {
+                clientMutationId
+              }
+            }`,
+            { commentId: comment.id }
+          );
+        }
+      }
+      execute = pageInfo.hasNextPage;
+      nextCursor = pageInfo.endCursor;
+    }
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
