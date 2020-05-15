6e1c9747cca035ea2a42352deb056e0fd919ab0f
Marcin Dobosz <mdobosz@istreamplanet.com>
2020-05-14 17:53:35 -0700

    detect changed files

--- build-workspace-matrix/index.ts
+++ build-workspace-matrix/index.ts
@@ -1,18 +1,63 @@
 import * as glob from "@actions/glob";
-import { getInput, setOutput, info } from "@actions/core";
+import { getInput, setOutput, info, setFailed } from "@actions/core";
+import { context, GitHub } from "@actions/github";
+import Webhooks from "@octokit/webhooks";
 
 const workspaceGlobs = getInput("workspace_globs", { required: true });
 const dependencyGlobs = getInput("dependency_globs");
 
+async function changedFiled(): Promise<string[]> {
+  const token = getInput("github-token", { required: true });
+  const github = new GitHub(token);
+
+  let head: string;
+  let base: string;
+  switch (context.eventName) {
+    case "push":
+      const pushPayload = context.payload as Webhooks.WebhookPayloadPush;
+      head = pushPayload.after;
+      base = pushPayload.before;
+      break;
+    case "pull_request":
+      const prPayload = context.payload as Webhooks.WebhookPayloadPullRequest;
+      head = prPayload.pull_request.head.sha;
+      base = prPayload.pull_request.base.sha;
+      break;
+    default:
+      setFailed(
+        `${context.eventName} is not supported when determining changed files.`
+      );
+      return [];
+  }
+
+  const { owner, repo } = context.repo;
+  const response = await github.repos.compareCommits({
+    owner,
+    repo,
+    head,
+    base,
+  });
+
+  if (response.status !== 200) {
+    setFailed(`GitHub API returned response with status ${response.status}`);
+    return [];
+  }
+  return response.data.files.map((file) => file.filename);
+}
+
 (async function run() {
-  const changedFiles: string[] = [];
+  const changedFiles = await changedFiled();
+
+  info(`Found changed files: ${changedFiles.join(", ")}`);
 
   const depsGlobber = await glob.create(dependencyGlobs);
   const dependencies = await depsGlobber.glob();
 
   info(`Found dependencies: ${dependencies.join(", ")}`);
 
-  const workspaceGlobber = await glob.create(workspaceGlobs, { implicitDescendants: false });
+  const workspaceGlobber = await glob.create(workspaceGlobs, {
+    implicitDescendants: false,
+  });
   const workspaces = await workspaceGlobber.glob();
 
   info(`Found matching workspaces: ${workspaces.join(", ")}`);
