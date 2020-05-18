df3e16f34007d61aaf5faa6b12827f5cc50f1021
Marcin Dobosz <43973855+mdobosz-isp@users.noreply.github.com>
2020-05-18 15:24:36 -0700

    feat(build-workspace-matrix): build dynamic work matrix based on globs and changed files (#4)

---
+++ build-workspace-matrix/README.md
@@ -0,0 +1,52 @@
+# build-workspace-matrix
+
+Builds a matrix of workspaces based on glob patterns and analysis of which files have changed
+
+## Inputs
+
+### `github-token`
+
+**Required** The GitHub API token to use.
+
+### `workspaces`
+
+**Required** A newline-separated list of globs representing specific workspaces.
+
+### `global_dependencies`
+
+A newline-separated list of globs representing dependencies of each workspace. If any of the dependencies have changed then all workspaces will be returned.
+
+## Outputs
+
+### `matrix`
+
+The matrix object of the following shape:
+
+```json
+{
+  "workspace": [
+    "match1",
+    "match2"
+  ]
+}
+```
+
+## Example usage
+
+````
+determine-matrix:
+  runs-on: ubuntu-latest
+  outputs:
+    matrix: ${{ steps.build-workspace-matrix.outputs.matrix }}
+  steps:
+  - uses: actions/checkout@master
+  - id: build-workspace-matrix
+    uses: iStreamPlanet/github-actions/build-workspace-matrix@master
+    with:
+      github-token: ${{secrets.GITHUB_TOKEN}}
+      workspace_globs: |
+        terraform/clusters/*/
+      dependency_globs: |
+        terraform/modules/**/*.tf
+        .github/workflows/terraform_diff.yml
+````
---
+++ build-workspace-matrix/index.ts
@@ -0,0 +1,100 @@
+import * as glob from "@actions/glob";
+import { getInput, setOutput, info, setFailed } from "@actions/core";
+import { context, GitHub } from "@actions/github";
+import Webhooks from "@octokit/webhooks";
+import { relative } from "path";
+import * as minimatch from "minimatch";
+
+(async function run() {
+  try {
+    const workspaceGlobs = getInput("workspaces", { required: true });
+    const workspaceGlobber = await glob.create(workspaceGlobs, {
+      implicitDescendants: false,
+    });
+    const workspaces = (await workspaceGlobber.glob()).map(makeRelative());
+
+    info(`Found matching workspaces: ${workspaces.join(", ")}`);
+
+    let result = workspaces;
+    if (context.eventName !== "schedule") {
+      const changedFiles = await changedFiled();
+      info(`Found changed files: ${changedFiles.join(", ")}`);
+      
+      let depsChanged = false;
+      const depsGlobsInput = getInput("global_dependencies");
+      if (depsGlobsInput.length > 0) {
+        for (const glob of depsGlobsInput.split("\n").map(g => g.trim())) {
+          if (glob.length === 0 || glob.startsWith("#")) {
+            continue;
+          }
+          if (changedFiles.some(minimatch.filter(glob))) {
+            info(`Found changed shared dependency matching glob '${glob}`)
+            depsChanged = true;
+            break;
+          }
+        }
+      }
+
+      if (depsChanged) {
+        result = workspaces;
+      } else {
+        result = workspaces.filter((w) =>
+          changedFiles.some((f) => f.startsWith(w))
+        );
+      }
+    }
+
+    console.log(
+      `Found ${result.length} impacted workspaces: ${result.join(", ")}`
+    );
+    setOutput("matrix", { workspace: result });
+  } catch (error) {
+    setFailed(error.message);
+  }
+})();
+
+async function changedFiled(): Promise<string[]> {
+  const token = getInput("github-token", { required: true });
+  const github = new GitHub(token);
+
+  let head: string;
+  let base: string;
+  switch (context.eventName) {
+    case "push":
+      const pushPayload = context.payload as Webhooks.WebhookPayloadPush;
+      head = pushPayload.after;
+      base = pushPayload.before;
+      break;
+    case "pull_request":
+      const prPayload = context.payload as Webhooks.WebhookPayloadPullRequest;
+      head = prPayload.pull_request.head.sha;
+      base = prPayload.pull_request.base.sha;
+      break;
+    default:
+      setFailed(
+        `${context.eventName} is not supported when determining changed files.`
+      );
+      return [];
+  }
+
+  const { owner, repo } = context.repo;
+  const response = await github.repos.compareCommits({
+    owner,
+    repo,
+    head,
+    base,
+  });
+
+  if (response.status !== 200) {
+    setFailed(`GitHub API returned response with status ${response.status}`);
+    return [];
+  }
+  return response.data.files.map((file) => file.filename);
+}
+
+function makeRelative() {
+  const cwd = process.cwd();
+  return function (path: string) {
+    return relative(cwd, path);
+  };
+}
