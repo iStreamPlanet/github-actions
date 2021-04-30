db45febf130d4c62cb40876f4dee716e0064912a
Charles Sullivan <csullivan@istreamplanet.com>
2021-04-30 08:55:03 -0600

    feat: Add a scanPath argument for TruffleHog
    Makes this more configurable.  Helpful when checking out multipe
    repositories and we can't make assumptions on the path.

--- trufflehog-actions-scan/entrypoint.sh
+++ trufflehog-actions-scan/entrypoint.sh
@@ -8,9 +8,15 @@ if [ -n "${INPUT_SCANARGUMENTS}" ]; then
   args="${INPUT_SCANARGUMENTS}" # Overwrite if new options string is provided
 fi
 
+githubRepo="file://$(pwd)" # Default target repository
+
+if [ -n "${INPUT_SCANPATH}" ]; then
+  githubRepo="${INPUT_SCANPATH}" # Overwrite if new options string is provided
+fi
+
 # By default the 'WORKDIR' of our Docker image is set to the 'GITHUB_WORKSPACE'
 # which is mounted into our image. This means, as long as a checkout action was
 # done before our action runs, we'll have access to the repository.
-githubRepo="file://$(pwd)" # Default target repository
+
 query="$args $githubRepo" # Build args query with repository url
 trufflehog $query
